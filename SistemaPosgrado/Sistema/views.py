fc4eb7e33217c67f2457badd0264ed6fab2fff60
Natsu-Efrain <natsusalamander8888@gmail.com>
2021-05-22 23:04:44 -0500

    Pruebas para la obtención de datos y validación con Login

--- SistemaPosgrado/Sistema/views.py
+++ SistemaPosgrado/Sistema/views.py
@@ -1,10 +1,26 @@
 from django.http.request import HttpHeaders, HttpRequest
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from Sistema.models import Usuario
 # Create your views here.
 # Prueba de commit Ian Mira
 #Metodo #1 de vizualización de Login
 def Login (request):
+    try:
+        #Se validan si se hace una petición en el formulario de Login
+        if(request.method == "POST"):
+            #Se obtiene los datos ingresados en el formulario
+            usuario = request.POST["Usuario"]
+            password = request.POST["Password"]
+            #Esta linea es igual a un Select * From Usuarios where='el nombre del usuario'
+            usuarios=Usuario.objects.get(Usuario=usuario)
+            #Se valida en los datos ingresados en la base de datos
+            if(usuarios.Usuario==usuario and usuarios.Contraseña==password):
+                return render(request,"gracias.html")
+            else:
+                return render(request,"fallo.html")
+    except:
+        return render(request,"fallo.html")
     return render(request,"Login.html")
 #Metodo de vizualización de Login
 def Formatos (request):
