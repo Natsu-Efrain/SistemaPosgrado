0576af50036871e7e6402cdf07e36733dedaa6ab
IAN PONCE <juan.perezlol@hotmail.com>
2021-05-23 11:41:58 -0500

    Ya me dio la conexion pero cambie el puerto y la contraseña, si no, no da

--- SistemaPosgrado/SistemaPosgrado/settings.py
+++ SistemaPosgrado/SistemaPosgrado/settings.py
@@ -80,10 +80,10 @@ DATABASES = {
     'ENGINE': 'django.db.backends.oracle',
     'NAME': 'SistemaPosgrado',
     'USER': 'SYSTEM',
-    'PASSWORD':'EFRAINdark_9',
+    'PASSWORD':'atenea',
     'NAME':'xe',
     'HOST':'localhost',
-    'PORT':'1521',
+    'PORT':'1522',
 }
 }
 
