3ff98d923f47289c7b0b649e93a7f763514c8760
Natsu-Efrain <natsusalamander8888@gmail.com>
2021-06-02 19:21:39 -0500

    cambios

--- SistemaPosgrado/Interfaz_General/models_usuario.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-# Create your models here.
-#Codigo para crear la tabla de Usuario
-class Usuario(models.Model):
-    Usuario=models.CharField(max_length=15,primary_key=True)
-    Contraseña=models.CharField(max_length=15)
--- SistemaPosgrado/Interfaz_General/views_InterfazGeneral.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from Interfaz_General.models_usuario import Usuario
-# Create your views here.
-#Metodo de vizualización de Modulos
-def Modulos (request):
-    return render(request,"Modulos.html")
