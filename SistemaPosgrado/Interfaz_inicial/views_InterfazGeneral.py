52791d5d72dc0fb059441068742e70222f77f1bd
Natsu-Efrain <natsusalamander8888@gmail.com>
2021-06-07 14:07:59 -0500

    Version.2.0

--- SistemaPosgrado/Interfaz_inicial/views_InterfazGeneral.py
+++ SistemaPosgrado/Interfaz_inicial/views_InterfazGeneral.py
@@ -4,10 +4,13 @@ from AlumnosApartado.models_Alumno import Alumno
 from ProfesoresApartado.models_personal import Personal
 from django.contrib.auth.models import User
 from SistemaPosgrado.funciones import CargarPermisos
+from AdminApartado.models_admin import Rol
+from django.core import serializers
 # Create your views here.
 #Metodo de vizualización de Modulos
 def Modulos (request):
-    return render(request,"Modulos.html")
+    Admi=Rol.objects.all()
+    return render(request,"Modulos.html",{'Roles':Admi})
 
 #Metodo de vizualización de Registro de Usuarios
 def Registro_U (request):
@@ -34,9 +37,11 @@ def Registro_U (request):
         try:
                 #Se hace validacion de los datos ingresados en el formulario en la base de datos
                 personal=Personal.objects.get(Num_Emp=Id)
+                Rol=personal.Rol
                 if(personal.Correo == Usser):
                         user = User.objects.create_user(Usser,'',Password)
-                        usuario=Usuario(Usser,Password)
+                        user.first_name = Rol
+                        usuario=Usuario(Usser,Password,Rol)
                         usuario.save()
                         user.save()
                         validacion="Exito"
@@ -61,7 +66,15 @@ def Menu_P_A (request):
         if(funcion=="CargarPagina"):
             prueba=request.POST["Prueba"]
             (p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19)=CargarPermisos(prueba)
-            return render(request,"Menu_P_A.html",{'p1':p1,'p2':p2,'p3':p3,'p4':p4,'p5':p5,'p6':p6,'p7':p7,'p8':p8,'p9':p9,'p10':p10,'p11':p11,'p12':p12,'p13':p13,'p14':p14,'p15':p15,'p16':p16,'p17':p17,'p18':p18,'p19':p19})
+            try:
+                    #Se hace validacion de los datos ingresados en el formulario en la base de datos
+                    alumno=Alumno.objects.get(Correo=prueba)
+                    nombre=alumno.Nombre_A+" "+alumno.Apellido_PA+" "+alumno.Apellido_MA
+            except:
+                    #Se hace validacion de los datos ingresados en el formulario en la base de datos
+                    personal=Personal.objects.get(Correo=prueba)
+                    nombre=personal.Nombre_P+" "+personal.Apellido_PP+" "+personal.Apellido_MP
+            return render(request,"Menu_P_A.html",{'nombre':nombre,'p1':p1,'p2':p2,'p3':p3,'p4':p4,'p5':p5,'p6':p6,'p7':p7,'p8':p8,'p9':p9,'p10':p10,'p11':p11,'p12':p12,'p13':p13,'p14':p14,'p15':p15,'p16':p16,'p17':p17,'p18':p18,'p19':p19})
     return render(request,"Menu_P_A.html")
 
 def Menu_P_Cor (request):
