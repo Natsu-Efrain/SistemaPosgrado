17cae2be52ab80b03c1a9699ca32a4b8ed355fbd
Natsu-Efrain <natsusalamander8888@gmail.com>
2021-06-08 07:26:36 -0500

    version 2.0

--- SistemaPosgrado/SistemaPosgrado/settings.py
+++ SistemaPosgrado/SistemaPosgrado/settings.py
@@ -81,18 +81,23 @@ WSGI_APPLICATION = 'SistemaPosgrado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#'default': {
+#    'ENGINE': 'django.db.backends.oracle',
+#    'NAME': 'SistemaPosgrado',
+#    'USER': 'SYSTEM',
+#    'PASSWORD':'EFRAINdark_9',
+#    'NAME':'xe',
+#    'HOST':'localhost',
+#    'PORT':'1521',
+#}
+#}
 DATABASES = {
-'default': {
-    'ENGINE': 'django.db.backends.oracle',
-    'NAME': 'SistemaPosgrado',
-    'USER': 'SYSTEM',
-    'PASSWORD':'EFRAINdark_9',
-    'NAME':'xe',
-    'HOST':'localhost',
-    'PORT':'1521',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
+    }
 }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
